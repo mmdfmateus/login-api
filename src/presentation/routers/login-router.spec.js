53b387798102e95d27d24263bff368b01eedba73
Mateus Mourão <mateusm@take.net>
2020-07-12 00:39:55 -0300

    ensure 400 if no password is provided

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -1,6 +1,6 @@
 class LoginRouter {
     route(request){
-        if(!request.body.email){
+        if(!request.body.email || !request.body.password){
             return {
                 statusCode: 400
             }
@@ -14,6 +14,18 @@ describe('Login Router', () => {
         const httpRequest = {
             body: {
                 password: 'any'
+            }
+        };
+        const httpResponse = sut.route(httpRequest);
+
+        expect(httpResponse.statusCode).toBe(400);
+    })
+
+    test('Should return 400 if no password is provided', () => {
+        const sut = new LoginRouter();
+        const httpRequest = {
+            body: {
+                email: 'any@mail.com'
             }
         };
         const httpResponse = sut.route(httpRequest);

8b2b4ed927384ccd0a9c18d2b28f58d7c478612c
Mateus Mourão <mateusm@take.net>
2020-07-15 00:01:31 -0300

    ensure useCase throws error if missing params

--- src/domain/usecases/auth-usecase.spec.js
+++ src/domain/usecases/auth-usecase.spec.js
@@ -1,7 +1,13 @@
+const { MissingParameterError } = require("../../utils/errors");
+
 class AuthUseCase {
-    async auth(email) {
+    async auth(email, password) {
         if(!email) {
-            throw new Error();
+            throw new MissingParameterError('email');
+        }
+
+        if(!password) {
+            throw new MissingParameterError('password');
         }
 
     }
@@ -12,6 +18,13 @@ describe('AuthUseCase', () => {
         const sut = new AuthUseCase();
         const promise = sut.auth();
 
-        expect(promise).rejects.toThrow();
+        expect(promise).rejects.toThrow(new MissingParameterError('email'));
+    });
+
+    test('Should throw if no password is provided', async () => {
+        const sut = new AuthUseCase();
+        const promise = sut.auth('any_mail@mail.com');
+
+        expect(promise).rejects.toThrow(new MissingParameterError('password'));
     });
 });

9fa750031508bcce0808f74c6b4f8be4ed0c7033
Mateus Mourão <mateusm@take.net>
2020-07-15 22:24:04 -0300

    ensure usecase calls repository with correct param

--- src/domain/usecases/auth-usecase.spec.js
+++ src/domain/usecases/auth-usecase.spec.js
@@ -1,6 +1,10 @@
 const { MissingParameterError } = require("../../utils/errors");
 
 class AuthUseCase {
+    constructor(loadUserByEmailRepository){
+        this.loadUserByEmailRepository = loadUserByEmailRepository;
+    }
+
     async auth(email, password) {
         if(!email) {
             throw new MissingParameterError('email');
@@ -10,6 +14,13 @@ class AuthUseCase {
             throw new MissingParameterError('password');
         }
 
+        await this.loadUserByEmailRepository.load(email);
+    }
+}
+
+class LoadUserByEmailRepository {
+    async load(email) {
+        this.email = email;
     }
 }
 
@@ -26,5 +37,14 @@ describe('AuthUseCase', () => {
         const promise = sut.auth('any_mail@mail.com');
 
         expect(promise).rejects.toThrow(new MissingParameterError('password'));
+    });
+
+    test('Should call LoadUserByEmailRepository with correct email', async () => {
+        const loadUserByEmailRepositorySpy = new LoadUserByEmailRepository();
+        const sut = new AuthUseCase(loadUserByEmailRepositorySpy);
+
+        await sut.auth('any_mail@mail.com', 'password');
+
+        expect(loadUserByEmailRepositorySpy.email).toBe('any_mail@mail.com');
     });
 });

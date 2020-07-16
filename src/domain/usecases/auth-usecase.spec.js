9c3cd68b96656b5c7c4d8f0b4011a1874cae7aaa
Mateus Mourão <mateusm@take.net>
2020-07-16 09:25:59 -0300

    refactor AuthUseCase constructor to receive an object

--- src/domain/usecases/auth-usecase.spec.js
+++ src/domain/usecases/auth-usecase.spec.js
@@ -5,7 +5,11 @@ const makeSut = () => {
     const encrypterSpy = makeEncrypter();
     const loadUserByEmailRepositorySpy = makeLoadUserByEmailRepository();
     const tokenGeneratorSpy = makeTokenGenerator();
-    const sut = new AuthUseCase(loadUserByEmailRepositorySpy, encrypterSpy, tokenGeneratorSpy);
+    const sut = new AuthUseCase({
+        loadUserByEmailRepository: loadUserByEmailRepositorySpy, 
+        encrypter: encrypterSpy, 
+        tokenGenerator: tokenGeneratorSpy
+    });
 
     return {
         encrypterSpy,
@@ -82,7 +86,7 @@ describe('AuthUseCase', () => {
     });
 
     test('Should throw if no LoadUserByEmailRepository is provided', async () => {
-        const sut = new AuthUseCase();
+        const sut = new AuthUseCase({});
 
         const promise = sut.auth('any_mail@mail.com', 'password');
 
@@ -90,7 +94,7 @@ describe('AuthUseCase', () => {
     });
 
     test('Should throw if LoadUserByEmailRepository has no load method', async () => {
-        const sut = new AuthUseCase({});
+        const sut = new AuthUseCase({loadUserByEmailRepository: {}});
 
         const promise = sut.auth('any_mail@mail.com', 'password');
 

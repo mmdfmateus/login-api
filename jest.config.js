29af1e1379efa2aafafaa1a04932c8eddff1239a
Mateus Mourão <mmdfmateus@gmail.com>
2020-07-18 12:46:07 -0300

    ensure LoadUserByEmailRepository returns an user if it exists

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,6 @@ module.exports = {
   coverageProvider: 'v8',
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
-  collectCoverageFrom: ['**/src/**/*.js']
+  collectCoverageFrom: ['**/src/**/*.js'],
+  preset: '@shelf/jest-mongodb'
 };
--- src/infra/repository/load-user-by-email-repository.spec.js
+++ src/infra/repository/load-user-by-email-repository.spec.js
@@ -1,6 +1,13 @@
+const { MongoClient } = require('mongodb');
+
 class LoadUserByEmailRepository {
+    constructor(userModel){
+        this.userModel = userModel;
+    }
+
     async load(email){
-        return null;
+        const user = await this.userModel.findOne({ email });
+        return user;
     }
 }
 
@@ -10,11 +17,43 @@ const makeSut = () => {
 }
 
 describe('LoadUserByEmail Repository', () => {
+    let connection;
+    let db;
+  
+    beforeAll(async () => {
+      connection = await MongoClient.connect(process.env.MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+      });
+      db = await connection.db();
+    });
+
+    beforeEach(async () => {
+        await db.collection('users').deleteMany();
+    });
+  
+    afterAll(async () => {
+      await connection.close();
+    });
+
     test('Should return null if no user is found', async () => {
-        const sut = makeSut();
+        const userModel = db.collection('users');
+        const sut = new LoadUserByEmailRepository(userModel);
 
         const user = await sut.load('invalid_email@mail.com');
 
         expect(user).toBeNull();
+    });
+
+    test('Should return an user if user is found', async () => {
+        const userModel = db.collection('users');
+        await userModel.insertOne({
+            email: 'valid_email@mail.com'
+        });
+        const sut = new LoadUserByEmailRepository(userModel);
+
+        const user = await sut.load('valid_email@mail.com');
+
+        expect(user.email).toBe('valid_email@mail.com');
     });
 });

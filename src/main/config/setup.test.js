7fc1c4e4760d76bf17190d82db37e6c2c8e74638
Mateus Mourão <mmdfmateus@gmail.com>
2020-07-19 00:40:32 -0300

    ensure CORS is enable

--- src/main/config/setup.test.js
+++ src/main/config/setup.test.js
@@ -7,7 +7,18 @@ describe('App Setup', () => {
             res.send('');
         })
         
-        const res = await request(app).get('/');
+        const res = await request(app).get('/test/x_powered_by');
         expect(res.headers['x-powered-by']).toBeUndefined();
+    });
+
+    test('Should enable CORS', async () => {
+        app.get('/test/cors', (req, res) => {
+            res.send('');
+        })
+        
+        const res = await request(app).get('/test/cors');
+        expect(res.headers['access-control-allow-origin']).toBe('*');
+        expect(res.headers['access-control-allow-methods']).toBe('*');
+        expect(res.headers['access-control-allow-headers']).toBe('*');
     });
 });

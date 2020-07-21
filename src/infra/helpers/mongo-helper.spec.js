91489c9c7a8fdd337a40f543ba521848c50fb930
Mateus Mourão <mmdfmateus@gmail.com>
2020-07-20 23:48:00 -0300

    fix stuck jest script when mongo wasnt disconnected

--- src/infra/helpers/mongo-helper.spec.js
+++ src/infra/helpers/mongo-helper.spec.js
@@ -1,9 +1,15 @@
-const MongoHelper = require('./mongo-helper');
+const sut = require('./mongo-helper');
 
 describe('Mongo Helper', () => {
-    test('Should reconnect when calls getdb() and the client is disconnected', async () => {
-        const sut = MongoHelper;
+    beforeAll(async () => {
         await sut.connect(process.env.MONGO_URL);
+    });
+
+    afterAll(async () => {
+        await sut.disconnect();
+    });
+
+    test('Should reconnect when calls getdb() and the client is disconnected', async () => {
         expect(sut.db).toBeTruthy();
         
         await sut.disconnect();

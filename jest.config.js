a650039e86571f7e9f7ec765836b080b4ca9a007
Mateus Mourão <mmdfmateus@gmail.com>
2020-07-20 23:55:21 -0300

    ignore main files in jest code coverage

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,6 @@ module.exports = {
   coverageProvider: 'v8',
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
-  collectCoverageFrom: ['**/src/**/*.js'],
+  collectCoverageFrom: ['**/src/**/*.js', '!**/src/main/**.js'],
   preset: '@shelf/jest-mongodb'
 };
